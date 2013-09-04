35bd1ad36d6e8615afd355c4659da7a8ae33d89b
Cyranix <andrew.m.harrison@gmail.com>
2013-09-04 16:23:07 -0700

    Second pass at JS cleanup (mostly indentation)

--- crypto.js
+++ crypto.js
@@ -17,114 +17,116 @@
         guessMapping,
         playerInfoElement,
         playAgainButton,
+        guessFrom,
+        guessTo,
         alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
 
     $(function init() {
-	    playerInfoElement = $('<p>').hide().appendTo('body');
-	    setUpEncoding();
+        playerInfoElement = $('<p>').hide().appendTo('body');
+        playAgainButton = $('<button>').text('Play again').click(setUpEncoding).hide().appendTo('body');
+        blankQuote = $("#encrypted-quote-blank");
+        guessFrom = $("#guess-from");
+        guessTo = $("#guess-to")
+
+        setUpEncoding();
+
+        $('#guess-button').on('click', processGuess); // TODO: convert to proper event handler
     });
-	
-	// TODO: refactor into one-time setup and reset functions
-	function setUpEncoding() {
-	    if (playAgainButton !== undefined && playAgainButton.is(':visible')) {
-		    playAgainButton.hide();
-	    }
 
-	    encoding = generateEncoding();
-	    blankQuote = $("#encrypted-quote-blank");
+    // TODO: refactor into one-time setup and reset functions
+    function setUpEncoding() {
+        if (playAgainButton !== undefined && playAgainButton.is(':visible')) {
+            playAgainButton.hide();
+        }
 
-	    displayQuote("a");
-    	$('#guess-button').on('click', processGuess); // TODO: convert to proper event handler
-    	guessMapping = {};
+        encoding = generateEncoding();
+
+        displayQuote("a");
+        guessMapping = {};
     }
 
-	// Displays the encrypted quote and blank quote to be filled in.
+    // Displays the encrypted quote and blank quote to be filled in.
     function displayQuote(quote) {
-	    encryptQuote(quote);
-	    $("#encrypted-quote").html(encryptedQuote);
-	    blankQuote.html(quoteDisplay); // TODO: avoid html()
+        encryptQuote(quote);
+        $("#encrypted-quote").html(encryptedQuote);
+        blankQuote.html(quoteDisplay); // TODO: avoid html()
     }
 
     // Handles a player's guess.
     function processGuess(a,b) {
-	    if (playerInfoElement.is(':visible')) { 
-		    playerInfoElement.hide();
-	    }
-	    var guessEncryptedLetter = $("#guess-from").val();
-	    var guessRealLetter = $("#guess-to").val();
-	    if (guessEncryptedLetter.length != 1 || guessRealLetter.length != 1) {
-		    playerInfoElement.text("guesses must be single letters").css('color', 'crimson').show(); // TODO: kill css()
-		    return;
-	    }
-	
-	    guessMapping[guessEncryptedLetter] = guessRealLetter;
+        if (playerInfoElement.is(':visible')) { 
+            playerInfoElement.hide();
+        }
+        var guessEncryptedLetter = guessFrom.val();
+        var guessRealLetter = guessTo.val();
+        if (guessEncryptedLetter.length != 1 || guessRealLetter.length != 1) {
+            playerInfoElement.text("guesses must be single letters").css('color', 'crimson').show(); // TODO: kill css()
+            return;
+        }
+        
+        guessMapping[guessEncryptedLetter] = guessRealLetter;
 
-	    for (var i = 0; i < encryptedQuote.length; i++) {
-    		var encryptedLetter = encryptedQuote.charAt(i);
-    		//if mapping contains encrypted letter,  set character at x in quote display to mapping[encryptedLetter]
-    		if (encryptedLetter in guessMapping) {
-    			quoteDisplay = quoteDisplay.substring(0, i) + guessMapping[encryptedLetter] + quoteDisplay.substring(i+1);
-    		}
-	    }
-    	//reset quote display text element
-    	// blankQuote.html(quoteDisplay);
-    	blankQuote.html().replace();
+        for (var i = 0; i < encryptedQuote.length; i++) {
+            var encryptedLetter = encryptedQuote.charAt(i);
+            //if mapping contains encrypted letter,  set character at x in quote display to mapping[encryptedLetter]
+            if (encryptedLetter in guessMapping) {
+                quoteDisplay = quoteDisplay.substring(0, i) + guessMapping[encryptedLetter] + quoteDisplay.substring(i+1);
+            }
+        }
+        //reset quote display text element
+        // blankQuote.html(quoteDisplay);
+        blankQuote.html().replace();
     
-    	//if no "_" then check if they are correct
-    	if (quoteDisplay.indexOf("_") == -1) {
-    		if (checkWinCondition()) {
-    			playerInfoElement.text("You figured it out!").css('color', 'MidnightBlue').show(); // TODO: kill css()
-    			if (playAgainButton === undefined) {
-    				playAgainButton = $('<button>').text('Play again').click(setUpEncoding).appendTo('body');
-    			}
-    			playAgainButton.show();
-    		}
+        //if no "_" then check if they are correct
+        if (quoteDisplay.indexOf("_") == -1) {
+            if (checkWinCondition()) {
+                playerInfoElement.text("You figured it out!").css('color', 'MidnightBlue').show(); // TODO: kill css()
+                playAgainButton.show();
+            }
         }
     }
 
     // Checks whether the evaluated quote matches the actual quote.
-	function checkWinCondition() {
-		for(var i = 0; i < quoteDisplay.length; i++) {
-			if (quoteDisplay.charAt(i) == " ") {
-				continue;
-			}
-			if (encryptedQuote.charAt(i) != encoding[quoteDisplay.charAt(i)]) {
-				console.log("encrypted letter: " + encryptedQuote.charAt(i) + " real letter: " + quoteDisplay.charAt(i) + " " + encoding[quoteDisplay.charAt(i)]);
-				console.log(encoding);
-				return false;
-			}
-		}
-		return true;
-	}
+    function checkWinCondition() {
+        for (var i = 0; i < quoteDisplay.length; i++) {
+            if (quoteDisplay.charAt(i) == " ") {
+                continue;
+            }
+            if (encryptedQuote.charAt(i) != encoding[quoteDisplay.charAt(i)]) {
+                console.log("encrypted letter: " + encryptedQuote.charAt(i) + " real letter: " + quoteDisplay.charAt(i) + " " + encoding[quoteDisplay.charAt(i)]);
+                console.log(encoding);
+                return false;
+            }
+        }
+        return true;
+    }
 
     // Generates an encoding of real letters to encrypted letters.
     function generateEncoding() {
-	    encoding = {};
-		keys = alphabet.slice(0);
-		while (alphabet.length > 0) {
-			var randomAlpha = Math.floor(Math.random() * alphabet.length);
-			encoding[keys[0]] = alphabet[randomAlpha];
-			keys.splice(0,1);
-			alphabet.splice(randomAlpha, 1);
-		}
-		return encoding;
-  }
+        encoding = {};
+        var keys = alphabet.slice(0);
+        while (alphabet.length > 0) {
+            var randomAlpha = Math.floor(Math.random() * alphabet.length);
+            encoding[keys[0]] = alphabet[randomAlpha];
+            keys.splice(0,1);
+            alphabet.splice(randomAlpha, 1);
+        }
+        return encoding;
+    }
 
     // Generate quote and encrypted quote (set puzzle)
-	function encryptQuote(rawQuote) {
-		encryptedQuote = "";
-		quoteDisplay = "";
+    function encryptQuote(rawQuote) {
+        encryptedQuote = "";
+        quoteDisplay = "";
 
-		for (var i = 0, len = rawQuote.length; i < len; i++) {
-			if (rawQuote[i] == " ") {
-				encryptedQuote = encryptedQuote + " ";
-				quoteDisplay = quoteDisplay + " ";
-				continue;
-			}
-	    	encryptedQuote = encryptedQuote + encoding[rawQuote[i]];
-			quoteDisplay = quoteDisplay + "_";
-		}
-	}
-
-
+        for (var i = 0, len = rawQuote.length; i < len; i++) {
+            if (rawQuote[i] == " ") {
+                encryptedQuote = encryptedQuote + " ";
+                quoteDisplay = quoteDisplay + " ";
+                continue;
+            }
+            encryptedQuote = encryptedQuote + encoding[rawQuote[i]];
+            quoteDisplay = quoteDisplay + "_";
+        }
+    }
 })();
